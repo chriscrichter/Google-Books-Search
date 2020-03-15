ab327456608b69442f724781ba14dcd9bd14bd33
chriscrichter <chris_richter@outlook.com>
2020-03-15 09:40:36 -0400

    Adding more folders and files.

---
+++ routes/api/books.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/booksController");
+
+// Matches with "/api/books"
+router.route("/")
+  .get(booksController.findAll)
+  .post(booksController.create);
+
+// Matches with "/api/books/:id"
+/* router
+  .route("/:id")
+  .get(booksController.findById)
+  .put(booksController.update)
+  .delete(booksController.remove); */
+
+// Matches with "/api/books/:title"
+router
+  .route("/:title")
+  .get(booksController.findMany)
+
+module.exports = router;
